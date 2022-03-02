020cb1c2e6cf2dc37cd6b30d6393853698fcb263
LeandroMarcelo <lmarcelos2019@gmail.com>
2022-03-02 14:32:22 -0300

    funciones para la DB

--- config/db.js
+++ config/db.js
@@ -8,3 +8,65 @@ const connection = mysql.createConnection({
   password: config.db_password,
   database: config.db_name,
 });
+
+function query(sql, data) {
+  return new Promise((resolve, reject) => {
+    connection.query(sql, data, function (error, result) {
+      error ? reject(error.sqlMessage) : resolve(result);
+    });
+  });
+}
+
+async function getAll() {
+  try {
+    const products = await query(`SELECT * FROM products`);
+    return products;
+  } catch (error) {
+    return { error, success: false };
+  }
+}
+
+async function likeName(tableName, input) {
+  try {
+    return await query(
+      `SELECT * FROM ${tableName} WHERE name LIKE "%${input}%"`
+    );
+  } catch (error) {
+    return error;
+  }
+}
+
+async function limitando(tableName, startingLimit, resultsPerPage) {
+  try {
+    return await query(
+      `SELECT * FROM ${tableName} LIMIT ${startingLimit}, ${resultsPerPage}`
+    );
+  } catch (error) {
+    return error;
+  }
+}
+async function filterCategory(tableName, idCategory) {
+  try {
+    return await query(
+      `SELECT * FROM ${tableName} WHERE idCategory=${idCategory}`
+    );
+  } catch (error) {
+    return error;
+  }
+}
+
+async function sortingName(tableName, sort_by) {
+  try {
+    return await query(`SELECT * FROM ${tableName} order by name ${sort_by}`);
+  } catch (error) {
+    return error;
+  }
+}
+
+module.exports = {
+  getAll,
+  likeName,
+  limitando,
+  filterCategory,
+  sortingName,
+};
